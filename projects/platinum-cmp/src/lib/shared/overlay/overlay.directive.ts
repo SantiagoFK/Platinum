b15db982eee457e271f41037a979da36b75ddc0b
santi <santiagofb932@gmail.com>
2024-12-07 22:20:19 -0300

    build: created overlay directive files and SlideModal files

---
+++ projects/platinum-cmp/src/lib/shared/overlay/overlay.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[libOverlay]'
+})
+export class OverlayDirective {
+
+  constructor() { }
+
+}
--- projects/platinum-cmp/src/public-api.ts
+++ projects/platinum-cmp/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of platinum-cmp
  */
 
-export * from './lib/mobile/overlay/slide-modal/index';
+export * from './lib/mobile/slide-modal/index';
+export * from './lib/shared/overlay/index';
